0426bc9de94ebd74918ad128703779ae92cfe323
Chad Hogg <chadhogg@gmail.com>
2022-02-23 13:18:27 -0500

    Explored two different data structures for Supercomputer, still am not fast enough.

--- c++/spring2022/Supercomputer/Supercomputer.cpp
+++ c++/spring2022/Supercomputer/Supercomputer.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <list>
 #include <cassert>
+#include <set>
 
 enum Type { FLIP, QUERY };
 
@@ -224,8 +225,145 @@ void solve (Problem & prob) {
     }
 }
 
+void alternSolve (Instructions const& instructions) {
+    std::set<int> onBits;
+    for (Instruction const& inst : instructions) {
+        if (inst.type == FLIP) {
+            if (onBits.find (inst.flipBit) != onBits.end ()) {
+                onBits.erase (inst.flipBit);
+            }
+            else {
+                onBits.insert (inst.flipBit);
+            }
+        }
+        else {
+            int count = 0;
+            std::set<int>::const_iterator iter = onBits.lower_bound (inst.startBit);
+            std::set<int>::const_iterator stop = onBits.upper_bound (inst.endBit);
+            while (iter != stop) {
+                ++count;
+                ++iter;
+            }
+            std::cout << count << "\n";
+        }
+    }
+}
+
+struct OnRange {
+    int firstBit;
+    int lastBit;
+};
+
+using OnRanges = std::list<OnRange>;
+/*
+1-1	3-7	10-12	14-18	20-20
+
+keep iterating as long as flip > last
+
+searching for 0, stops on 1-1 and extends 1-1 to 0-1
+searching for 1, stops on 1-1 and erases it
+searching for 2, stops on 3-7 and merges 1-1 with 3-7 into 1-7
+searching for 3, stops on 3-7 and shrinks it to 4-7
+searching for 4, stops on 3-7 and splits it to 3-3 and 5-7
+searching for 5, stops on 3-7 and splits it to 3-4 and 6-7
+searching for 6, stops on 3-7 and splits it to 3-5 and 7-7
+searching for 7, stops on 3-7 and shrinks it to 3-6
+searching for 8, stops on 10-12 and extends 3-7 to 3-8
+searching for 9, stops on 10-12 and extends 10-12 to 9-12
+searching for 10, stops on 10-12 and shrinks it to 11-12
+searching for 11, stops on 10-12 and splits it to 10-10 and 12-12
+searching for 12, stops on 10-12 and shrinks it to 10-11
+searching for 13, stops on 14-18 and merges 10-12 with 14-18 into 10-18
+searching for 14, stops on 14-18 and shrinks it to 15-18
+searching for 15, stops on 14-18 and splits it to 14-14 and 16-18
+searching for 16, stops on 14-18 and splits it to 14-15 and 17-18
+searching for 17, stops on 14-18 and splits it to 14-16 and 18-18
+searching for 18, stops on 14-18 and shrinks it to 14-17
+searching for 19, stops on 20-20 and merges 14-18 with 20-20 into 14-20
+searching for 20, stops on 20-20 and erases it
+searching for 21, stops at end and adds 21-21
+*/
+void processFlip2 (OnRanges & onRanges, Instruction const& inst) {
+    OnRanges::iterator iter = onRanges.begin ();
+    // Find the range that includes this bit.
+    while (iter != onRanges.end () && inst.flipBit > iter->lastBit) {
+        ++iter;
+    }
+    // If we got all the way to the end of the list, add a new range at the end.
+    if (iter == onRanges.end ()) {
+        onRanges.push_back ({inst.flipBit, inst.flipBit});
+    }
+    // Or if it was the only one in a singleton range, it gets removed from the list.
+    else if (iter->firstBit == inst.flipBit && iter->lastBit == inst.flipBit) {
+        onRanges.erase (iter);
+    }
+    // Or if it was the first one in a larger range, shrink that range.
+    else if (iter->firstBit == inst.flipBit && iter->lastBit > inst.flipBit) {
+        ++(iter->firstBit);
+    }
+    // Or if it was the last one in a larger range, shrink that range.
+    else if(iter->firstBit < inst.flipBit && iter->lastBit == inst.flipBit) {
+        --(iter->lastBit);
+    }
+    // Or if it was in the middle of a range, split that range in two.
+    else if(iter->firstBit < inst.flipBit && iter->lastBit > inst.flipBit) {
+        int first = iter->firstBit;
+        iter->firstBit = inst.flipBit + 1;
+        onRanges.insert (iter, {first, inst.flipBit - 1});
+    }
+    // Or this means that the bit was off, and we are turning it on.
+    else {
+        OnRanges::iterator prev {iter};
+        --prev;
+        // If this has a range directly before and another directly after it, merge them.
+        if (prev != onRanges.end () && prev->lastBit == inst.flipBit - 1 && iter->firstBit == inst.flipBit + 1) {
+            prev->lastBit = iter->lastBit;
+            onRanges.erase (iter);
+        }
+        // Or if this only has a range directly before it, extend it to include this bit.
+        else if (prev != onRanges.end () && prev->lastBit == inst.flipBit - 1) {
+            ++(prev->lastBit);
+        }
+        // Or if this only has a range directly after it, extend it to include this bit.
+        else if (iter->firstBit == inst.flipBit + 1) {
+            --(iter->firstBit);
+        }
+        // Or just make a new singleton range.
+        else {
+            onRanges.insert (iter, {inst.flipBit, inst.flipBit});
+        }
+    }
+}
+
+
+void processQuery2 (OnRanges & onRanges, Instruction const& inst) {
+    int total = 0;
+    OnRanges::iterator iter = onRanges.begin ();
+    // Loop through every range that is before the end of the target
+    while (iter != onRanges.end () && iter->firstBit <= inst.endBit) {
+        // If there is an overlap, add to the total
+        total += std::max (0, std::min (inst.endBit, iter->lastBit) - std::max (inst.startBit, iter->firstBit) + 1); 
+        ++iter;
+    }
+    std::cout << total << "\n";
+}
+
+void solve3 (Instructions const& instructions) {
+    OnRanges onRanges;
+    for (Instruction const& inst : instructions) {
+        if (inst.type == FLIP) {
+            processFlip2 (onRanges, inst);
+        }
+        else {
+            processQuery2 (onRanges, inst);
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     Problem prob = readProblem ();
-    solve (prob);
+    //solve (prob);
+    alternSolve (prob.instructions);
+    //solve3 (prob.instructions);
     return 0;
 }
